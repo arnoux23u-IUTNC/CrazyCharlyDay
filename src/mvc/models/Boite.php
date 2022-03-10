01d4ea84f0fdc112b1f05826d72b74f90cac4b7c
xaerose <alexandre.germonville-bellet8@etu.univ-lorraine.fr>
2022-03-10 10:12:37 +0100

    add models Boite, Categorie, Produit

---
+++ src/mvc/models/Boite.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace customBox\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Boite extends Model
+{
+
+    protected $table = 'ccd_boite';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = false;
+
+}
