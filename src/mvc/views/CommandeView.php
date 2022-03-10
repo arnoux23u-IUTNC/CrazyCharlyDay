80d3075d6ff61965e71575af22693dc18849b83e
arnoux23u <arnouxguillaume54@hotmail.fr>
2022-03-10 17:26:40 +0100

    Create CommandeView.php

---
+++ src/mvc/views/CommandeView.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace custombox\mvc\views;
+
+use custombox\exceptions\ForbiddenException;
+use custombox\mvc\models\Commande;
+use custombox\mvc\models\Produit;
+use custombox\mvc\models\User;
+use custombox\mvc\Renderer;
+use custombox\mvc\View;
+use JetBrains\PhpStorm\Pure;
+use Slim\Container;
+use Slim\Http\Request;
+
+class CommandeView extends View
+{
+
+    private ?Commande $command;
+
+    #[Pure] public function __construct(Container $c, ?Commande $co, Request $request = null)
+    {
+        $this->command = $co;
+        parent::__construct($c, $request);
+    }
+
+    #[Pure] private function showHome(): string
+    {
+        $html = genererHeader("Accueil");
+        $html .= <<<HTML
+            BIENVENUE
+        </body>
+        </html>
+        HTML;
+        return $html;
+    }
+
+    protected function show():string
+    {
+        $products = "";
+        foreach($this->command->produits as $produit){
+            $products .= <<<HTML
+                <tr>
+                    <th scope="row">{$produit['id']}</th>
+                    <td>7</td>
+                    <td>7,223</td>
+                </tr>
+            HTML;
+        }
+        return genererHeader("Commande {$this->command['id']}").<<<HTML
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Commande {$this->command['id']}</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Produits</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Nom</th>
+                                    <th>Quantité</th>
+                                    <th>Prix</th>
+                                </tr>
+                                $products
+                            </thead>
+                            <tbody>
+        HTML;
+    }
+
+    /**
+     * @throws ForbiddenException
+     */
+    protected function create(): string
+    {
+        $productsHtml = "";
+        foreach (Produit::all() as $product) {
+            $productsHtml .= (new ProduitView($this->container, $product, $this->request))->render(Renderer::SHOW_IN_LIST_MIN);
+        }
+        return genererHeader("Création de commande",["commande.css"]) . <<<HTML
+            <form method="POST" action="{$this->container['router']->pathFor('creerCommande')}">
+                <h1>Créer une commande</h1>
+                <div style="display: grid; grid-template-columns: repeat(3, 1fr);">
+                    $productsHtml
+                </div>
+                <label for="color">Couleur boite</label>
+                <input type="color" name="color" id="color">
+                <input type="submit" value="Créer">
+            </form>
+        HTML;
+    }
+
+    public function render(int $method, int $access_level = Renderer::OTHER_MODE, $user = null): string
+    {
+        return match ($method) {
+            Renderer::REGISTER => $this->register(),
+            Renderer::CREATE => $this->create(),
+            default => parent::render($method, $access_level),
+        };
+    }
+}
