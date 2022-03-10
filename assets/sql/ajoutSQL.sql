2bd3c5e7b879b859b666ec80e42946d5138fc3c6
xaerose <alexandre.germonville-bellet8@etu.univ-lorraine.fr>
2022-03-10 10:33:04 +0100

    sql add base

---
+++ assets/sql/ajoutSQL.sql
@@ -0,0 +1,38 @@
+CREATE TABLE `ccd_user` (
+                            `id` int(11) NOT NULL,
+                            `name` varchar(255) NOT NULL,
+                            `mail` varchar(255) NOT NULL,
+                            `adresse` varchar(255) NOT NULL,
+                            `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `ccd_user`
+    ADD PRIMARY KEY (`id`);
+
+
+
+CREATE TABLE `ccd_commande` (
+                             `id` int(11) NOT NULL,
+                             `id_user` int(11) NOT NULL,
+                             `id_boite` int(11) NOT NULL,
+                             `paye` BOOLEAN
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `ccd_commande`
+    ADD PRIMARY KEY (`id`);
+ALTER TABLE `ccd_commande`
+    ADD CONSTRAINT `commande_fk_user` FOREIGN KEY (`id_user`) REFERENCES `ccd_user` (`id`);
+ALTER TABLE `ccd_commande`
+    ADD CONSTRAINT `commande_fk_boite` FOREIGN KEY (`id_boite`) REFERENCES `ccd_boite` (`id`);
+
+CREATE TABLE `ccd_contenucommande` (
+                                `id_commande` int(11) NOT NULL,
+                                `id_produit` int(11) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `ccd_contenucommande`
+    ADD CONSTRAINT `contenucommande_fk_commande` FOREIGN KEY (`id_commande`) REFERENCES `ccd_commande` (`id`);
+ALTER TABLE `ccd_contenucommande`
+    ADD CONSTRAINT `contenucommande_fk_boite` FOREIGN KEY (`id_produit`) REFERENCES `ccd_produit` (`id`);
+ALTER TABLE `ccd_contenucommande`
+    ADD PRIMARY KEY (`id-commande,id_produit`);
